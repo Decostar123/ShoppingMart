aa0c3a8f4568500387a00c84477669216af2a6c8
Decostar Sharma <decostarsharma113@gmail.com>
2023-03-01 11:02:31 +0530

    deployed

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 const { MONGOURI } = require("./config/keys");
+const path = require("path");
 const arr = [];
 const mongoose = require("mongoose");
 mongoose
-  .connect(
-    "mongodb+srv://Deco:ls8IAzyvrl2OHSvb@cluster0.s3pcave.mongodb.net/test"
-  )
+  .connect(MONGOURI)
   .then((resp) => console.log(" connections successful"))
   .catch((err) => console.log("connection failed "));
 //  last is the database name and below I have mentioned colection name which is cart
@@ -36,9 +35,16 @@ const port = process.env.PORT || 5000;
 // });
 // app.use(cors());
 app.use(express.json());
-app.get("/", function (req, res) {
-  res.send("hi");
+app.use(express.static(path.join(__dirname, "./client/build")));
+
+// if (process.env.NODE_ENV == "production") {
+
+console.log(path.join(__dirname, "./client/index.html"));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 });
+// }
 
 app.post("/login", (req, res) => {
   const mail = req.body.mail;
@@ -149,13 +155,4 @@ app.post("/signup", function (req, res) {
   // });
 });
 
-if (process.env.NODE_ENV == "production") {
-  const path = require("path");
-  console.log(path.resolve(__dirname, "build"));
-
-  app.get("/", (req, res) => {
-    app.use(express.static(path.resolve(__dirname, "build")));
-    res.sendFile(path.resolve(__dirname, "build", "index.html"));
-  });
-}
 app.listen(port, () => console.log(" server started on port ", port));
